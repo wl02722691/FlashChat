f785f92d4cbfa6fedd31b9f10edf937cbe9b8497
Alice <wl02722691@gmail.com>
2017-08-26 04:49:21 +0800

    Initial Commit

---
+++ Flash Chat/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat
+//
+//  This is the model class that represents the blueprint for a message
+
+class Message {
+    var sender : String = ""
+    var messageBody : String = ""
+    //TODO: Messages need a messageBody and a sender variable
+    
+    
+    
+}
